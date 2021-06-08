f2608d89bafb157fea33bdc05025553c1e17ca22
박성언 <parksungun@parksungun-ui-MacBookPro.local>
2021-06-08 22:55:57 +0900

    #0.4 Router and Styles

---
+++ src/routes/Detail.jsx
@@ -0,0 +1,3 @@
+// 폴더 생성 : mkdir src/routes
+// 파일 생성 : touch src/routes/Detail.jsx
+export default () => 'Detail';
---
+++ src/routes/Home.jsx
@@ -0,0 +1,4 @@
+// 폴더 생성 : mkdir src/routes
+// 파일 생성 : touch src/routes/Home.jsx
+
+export default () => 'Home';
